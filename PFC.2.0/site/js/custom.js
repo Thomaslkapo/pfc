d47221a3055d805fc8744578c65b32d7cf43fa9c
Thomas <55161463+Thomaslkapo@users.noreply.github.com>
2022-11-30 12:20:36 -0300

    js alteration

---
+++ PFC.2.0/site/js/custom.js
@@ -0,0 +1,10 @@
+$('.slick_slide').slick({
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	autoplay: true,
+    dots:true,
+    speed: 800,
+	autoplaySpeed: 2000,
+    prevArrow:'<i class="fas fa-angle-left  left_arrow">',
+    nextArrow:'<i class="fas fa-angle-right  right_arrow">',
+  });
